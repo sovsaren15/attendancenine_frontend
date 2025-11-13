0451679b276daa0e7e263d4b9afc56b0bc17e24f
sov saren <SovSaren>
2025-11-13 11:30:03 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import EmployeeRegistration from "./pages/EmployeeRegistration.jsx";
-import EmployeeList from "./pages/EmployeeList.jsx";
-import AttendanceScanner from "./pages/AttendanceScanner.jsx";
-import AttendanceRecord from "./pages/AttendanceRecord.jsx";
-import AttendancePage from "./pages/AttendancePage.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import TopPerformers from "./pages/TopPerformers.jsx";
 import Layout from "./components/Layout.jsx";
-import HomePage from "./pages/HomePage.jsx";
+
+const HomePage = React.lazy(() => import("./pages/HomePage.jsx"));
+const AttendanceScanner = React.lazy(() => import("./pages/AttendanceScanner.jsx"));
+const AttendanceRecord = React.lazy(() => import("./pages/AttendanceRecord.jsx"));
+const EmployeeRegistration = React.lazy(() => import("./pages/EmployeeRegistration.jsx"));
+const EmployeeList = React.lazy(() => import("./pages/EmployeeList.jsx"));
+const Dashboard = React.lazy(() => import("./pages/Dashboard.jsx"));
+const TopPerformers = React.lazy(() => import("./pages/TopPerformers.jsx"));
+const AttendancePage = React.lazy(() => import("./pages/AttendancePage.jsx"));
 
 function App() {
   return (
-    <Routes>
-      {/* Routes without Navbar */}
-      <Route path="/" element={<HomePage />} />
-      <Route path="/AttendanceScanner" element={<AttendanceScanner />} />
-      <Route path="/records" element={<AttendanceRecord />} />
-      {/* Routes with Navbar */}
-      <Route element={<Layout />}>
-        <Route path="/register" element={<EmployeeRegistration />} />
-        <Route path="/employees" element={<EmployeeList />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/top" element={<TopPerformers />} />
-        <Route path="/attendance" element={<AttendancePage />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {/* Routes without Navbar */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/AttendanceScanner" element={<AttendanceScanner />} />
+        <Route path="/records" element={<AttendanceRecord />} />
+        {/* Routes with Navbar */}
+        <Route element={<Layout />}>
+          <Route path="/register" element={<EmployeeRegistration />} />
+          <Route path="/employees" element={<EmployeeList />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/top" element={<TopPerformers />} />
+          <Route path="/attendance" element={<AttendancePage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
