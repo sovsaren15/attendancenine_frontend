0f22459d9a70c41105393821aaffe770fa83af35
sov saren <SovSaren>
2025-11-14 23:05:47 +0700

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -8,7 +8,7 @@ export const attendanceAPI = {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         employeeId,
-        type, // 'check-in' or 'check-out'
+        type, // 'check-in' or 'check-out's
       }),
     })
     if (!response.ok) {
